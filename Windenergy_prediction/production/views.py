1e9965a28a0dc192bf8259f916d2d16e4feef7c6
Muthukumar T <tmuthukumarnpm@gmail.com>
2020-07-04 14:33:03 +0530

    Map Functionality Modified

--- Windenergy_prediction/production/views.py
+++ Windenergy_prediction/production/views.py
@@ -39,22 +39,22 @@ def add_location_map(request):
     ld.save()
     loc_info = location_details.objects.filter(user_name= username).values('lattitude','longtitude','machine_name')
     latt = []
-    long = []
+    longi = []
     mn = []
     for i in loc_info:
         latt.append(i['lattitude'])
-        long.append(i['longtitude'])
+        longi.append(i['longtitude'])
         mn.append(i['machine_name'])
 
     loc_info1 = wind_details.objects.filter(username=username).values('latitude', 'longitude', 'orgname')
     for i in loc_info1:
         latt.append(i['latitude'])
-        long.append(i['longitude'])
+        longi.append(i['longitude'])
         mn.append(i['orgname'])
     #map = folium.Map(location=[lat,lon], zoom_start=6,tiles="Stamen Terrain")
     map = folium.Map()
     fg = folium.FeatureGroup(name="test")
-    for i, j, k in zip(latt, long, mn):
+    for i, j, k in zip(latt, longi, mn):
         fg.add_child(folium.Marker(location=[i,j], popup=k, icon=folium.Icon(color='green')))
     map.add_child(fg)
     map.save("Templates/map.html")
